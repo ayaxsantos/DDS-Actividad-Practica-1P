f443c3d14381c1fa600d2d01843cb59b78aacfa1
tbavutti <tomas.bavutti@gmail.com>
2021-08-22 18:58:04 -0300

    API 2.0

--- src/main/java/infraestructura/NotificadorMail.java
+++ src/main/java/infraestructura/NotificadorMail.java
@@ -2,6 +2,17 @@ package infraestructura;
 
 import dominio.Notificador;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.AddressException;
+
+
 public class NotificadorMail implements Notificador {
 
     private final String emailOrigen = "OkaEntregas@gmail.com";
@@ -9,7 +20,21 @@ public class NotificadorMail implements Notificador {
 
     @Override
     public void enviar(String origen, String destinatario, String asunto, String cuerpo){
+        Properties propiedades = new Properties();
+        Session session = Session.getDefaultInstance(propiedades);
 
+        try {
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(emailOrigen, "OKA")); //Setteas el que manda el mail
+            msg.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(destinatario, "Cliente")); //Setteas quien lo recibe
+            msg.setSubject(asunto);
+            msg.setText(cuerpo);
+            //Vamos a usar stmp para gmail
+            Transport transport = session.getTransport("smtp");
+            transport.connect(emailOrigen, remitente, clave);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
     }
 
 }
