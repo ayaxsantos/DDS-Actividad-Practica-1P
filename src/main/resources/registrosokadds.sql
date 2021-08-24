6dc89b5ea523cd60faba70d35584962154f8de5e
ayaxsantos <ayax.asantos@gmail.com>
2021-08-24 19:52:07 -0300

    Actualizo scripts db

--- src/main/resources/registrosokadds.sql
+++ src/main/resources/registrosokadds.sql
@@ -2,31 +2,20 @@ CREATE DATABASE registrosoka;
 
 USE registrosoka;
 
-CREATE TABLE `destinatario` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `apellido` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `nombre` varchar(15) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `destino` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `calle` varchar(50) NOT NULL,
-  `localidad` varchar(100) NOT NULL,
-  `numero` int NOT NULL,
-  `destinatario_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_dihwcbhtxh66p801xm3x712oo` (`destinatario_id`),
-  CONSTRAINT `FK_dihwcbhtxh66p801xm3x712oo` FOREIGN KEY (`destinatario_id`) REFERENCES `destinatario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 CREATE TABLE `envio` (
   `id` int NOT NULL AUTO_INCREMENT,
   `fechaDevolucion` tinyblob,
   `pesoTotal` int DEFAULT NULL,
-  `destinatario_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `paquete` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `peso` int NOT NULL,
+  `tamanio` int DEFAULT NULL,
+  `envio_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_jdo33lvhxfpkqqprc3aoh3vjt` (`destinatario_id`),
-  CONSTRAINT `FK_jdo33lvhxfpkqqprc3aoh3vjt` FOREIGN KEY (`destinatario_id`) REFERENCES `destinatario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `FK_anqqmqswlq1970wt3g6wty8ko` (`envio_id`),
+  CONSTRAINT `FK_anqqmqswlq1970wt3g6wty8ko` FOREIGN KEY (`envio_id`) REFERENCES `envio` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
